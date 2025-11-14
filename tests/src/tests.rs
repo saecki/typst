6ba8ee89c0ca5ae4d3b5e3c0ed6a4af664e33057
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2025-11-14 13:57:08 +0100

    Generate static html test report

--- tests/src/tests.rs
+++ tests/src/tests.rs
@@ -6,6 +6,7 @@ mod custom;
 mod logger;
 mod output;
 mod pdftags;
+mod report;
 mod run;
 mod world;
 
@@ -147,7 +148,13 @@ fn test() {
     run::update_hash_refs::<output::Pdf>(&hashes);
     run::update_hash_refs::<output::Svg>(&hashes);
 
-    let passed = logger.into_inner().finish();
+    let mut logger = logger.into_inner();
+
+    logger.reports.sort_by(|a, b| a.name.cmp(&b.name));
+    let html = report::html::generate(&logger.reports);
+    std::fs::write(Path::new(STORE_PATH).join("report.html"), html).unwrap();
+
+    let passed = logger.finish();
     if !passed {
         std::process::exit(1);
     }
@@ -221,6 +228,7 @@ fn run_parser_test(
         errors: String::new(),
         infos: String::new(),
         mismatched_output: false,
+        report: None,
     };
 
     let syntax_file = live_path.join(format!("{}.syntax", test.name));
