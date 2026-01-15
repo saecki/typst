3a7ab4a27f60a85367893efd14c9e60e4961dc39
Tobias Schmitz <tobiasschmitz2001@gmail.com>
2026-01-15 17:55:49 +0100

    Add eval tests and empty attribute

--- tests/src/args.rs
+++ tests/src/args.rs
@@ -102,11 +102,6 @@ impl CliArguments {
             })
         })
     }
-
-    /// Whether the stage should be run depending on the `--stages` flag.
-    pub fn should_run(&self, stage: TestStages) -> bool {
-        self.stages().intersects(stage)
-    }
 }
 
 /// What to do.
